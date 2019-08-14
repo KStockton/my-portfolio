91cdb763a6c3a5d02dd787176d80439e11e7cb42
Michael KS <kingstockton@gmail.com>
2019-08-14 11:52:03 -0600

    Install sass

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom'
-import resume from './resume.pdf';
-import './App.css';
-import Nav from './Nav/Nav'
-import Home from './Home/Home'
-import Portfolio from './Portfolio/Portfolio';
-import Contact from './Contact/Contact'
+import resume from '../resume.pdf';
+import './App.scss';
+import Nav from '../Nav/Nav'
+import Home from '../Home/Home'
+import Portfolio from '../Portfolio/Portfolio';
+import Contact from '../Contact/Contact'
 
 export default class App extends Component {
   constructor() {
@@ -14,6 +14,7 @@ export default class App extends Component {
     }
   }
   render() {
+ 
     return (
       <div>
         <div className="App">
