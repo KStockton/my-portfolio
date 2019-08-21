ef2a36f4f56d4436cbd9872ebb05d252e2d80e10
Michael KS <kingstockton@gmail.com>
2019-08-21 01:26:23 -0600

    Fix top position

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,21 +7,22 @@ const Home = () => {
 
   return (
     <div id="home">
-      <div className="Home-wrapper">
         <header>
           <h1>Michael</h1>
-          <p>King<span>~</span>Stockton</p>
+          <p>King<span>-</span>Stockton</p>
         </header>
-      <ul>
-      <li><a href="https://github.com/KStockton">
-        <FontAwesomeIcon className="Home-fontawesome" icon={faGithub}/> 
-      </a></li>
-      <li><a href="https://www.linkedin.com/in/michaelkingstockton/">
-        <FontAwesomeIcon  className="Home-fontawesome" icon={faLinkedin}/>
-      </a></li>
+      <ul className="resource-links">
+        <li>
+          <a href="https://github.com/KStockton">
+          <FontAwesomeIcon className="Home-fontawesome" icon={faGithub}/> 
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/michaelkingstockton/">
+          <FontAwesomeIcon  className="Home-fontawesome" icon={faLinkedin}/>
+          </a>
+        </li>
       </ul>
-      </div >
-
     </div>
   )
 
