8a62bbb8bafbf98142058b121bcc24f01f7d8d6e
Michael KS <kingstockton@gmail.com>
2019-08-25 16:26:00 -0600

    Move footer to its own component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
 import Nav from '../Nav/Nav'
 import Home from '../Home/Home';
+import Footer from '../Footer/Footer';
 import Portfolio from '../Portfolio/Portfolio';
-import Contact from '../Contact/Contact'
+import Contact from '../Contact/Contact';
 
-export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-    }
-  }
-  render() {
+const App = () => {
  
-    return (
-      <div>
-        <div className="App">
-        <Nav/>
+  return (
+    <div>
+      <Nav/>
       <Switch>
         <Route  exact path="/" component={Home} />
         <Route path="/portfolio" component={Portfolio} />
         <Route exact path="/contact" component={Contact}/>
       </Switch>
+      <Footer/>
     </div>
-      </div>
-    )
-  }
+  )
 }
+
+export default App;
