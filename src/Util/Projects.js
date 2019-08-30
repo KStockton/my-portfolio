9c294ec96f9c87a3ae0f3becc628d67a3a55940c
Michael KS <kingstockton@gmail.com>
2019-08-30 15:17:52 -0600

    Fix source and live links

--- src/Util/Projects.js
+++ src/Util/Projects.js
@@ -7,64 +7,48 @@ export const portfolioInfo =
     {
       gitHubLink: 'https://github.com/KStockton/Vue.js-Synonyms-v1',
       liveLink: 'https://thesaurus-challenge.herokuapp.com/',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'Synonyms',
       description: 'A Thesaurus in Vue'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/Trapper-Keeper',
       liveLink: 'https://trapper-keeper-front.herokuapp.com/',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'Trapper Keeper',
       description: 'Create notes and list with Trapper Keeper'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/Newswired',
       liveLink: 'https://kstockton.github.io/Newswired',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'News Wired',
       description: 'Keep up with the latest news'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/pocket_ramiro_react',
       liveLink: 'https://pocket-ramiro-react.herokuapp.com/',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'Pocket Ramiro',
       description: 'Track issues and tickets for Facility Equipment'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/Movie-Tracker',
       liveLink: 'https://movie-tracker-fe.herokuapp.com',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'Movie Tracker',
       description: 'View and save the latest movies'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/climb-on',
-      liveLink: '',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
+      liveLink: 'https://kstockton.github.io/climb-on/',
       name: 'Climb On',
       description: 'View the closest climibing trails'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/Build-Your-Own-Backend',
       liveLink: 'https://trapper-keeper-front.herokuapp.com/',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'Backend NBA Draft 2019',
       description: 'NBA Draft Players and Teams in Express'
     }, 
     {
       gitHubLink: 'https://github.com/KStockton/swapi-box',
       liveLink: 'https://kstockton.github.io/swapi-box/',
-      gitHubIcon: 'fab fa-github',
-      liveIcon: 'fas fa-video',
       name: 'Swapi Box',
       description: 'Star Wars Characters, Vehicles and Planets'
     }],
